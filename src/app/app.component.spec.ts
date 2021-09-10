3645db954005f9e1bd070710c069444b6e49b66c
Salchiis <d1360_audi@hotmail.com>
2021-09-10 10:50:15 -0500

    fail

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,7 +22,7 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-leaflet-example');
   });
 
-   xit('should render title', () => {
+   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
